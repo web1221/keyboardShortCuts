71622e097deec75018bcd013c3653ade253ffcc3
Student <example@example.com>
2019-12-02 11:42:45 -0800

    get text editor to appear with no theme

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import './ace-builds/src-noconflict/ace.js'
+import './ace-builds/src-noconflict/ace.js';
+
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/javascript");
