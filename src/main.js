cd08493f13d2e11f9e742ac1073db83be6078808
Student <example@example.com>
2019-12-02 10:53:12 -0800

    test push on bb feature branch

--- src/main.js
+++ src/main.js
@@ -3,3 +3,4 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import './ace-builds/src-noconflict/ace.js'
+import { keyDetecter } from './keyDetect.js';
