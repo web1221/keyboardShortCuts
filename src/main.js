39d4ed8e1391a63f44db761e1b3fabd06830e567
Student <example@example.com>
2019-12-02 16:34:40 -0800

    add BB code snip for key press. Make changes to ace.js to start matching key-binding to atom key binding

--- src/main.js
+++ src/main.js
@@ -7,10 +7,18 @@ import './src-noconflict/theme-monokai.js';
 import './src-noconflict/mode-javascript.js';
 import './src-noconflict/worker-javascript.js';
 
-// ace.config.set('basePath', '/src-noconflict');
-// $(document).ready(function() {
-  var editor = ace.edit("editor");
-  editor.setTheme("ace/theme/monokai");
-  editor.session.setMode("ace/mode/javascript");
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/javascript");
 
-// });
+let collectionArray = [];
+
+document.addEventListener("keydown", function(event) {
+console.log(event.which);
+console.log(event.repeat);
+if (event.repeat === false) {
+    collectionArray.push(event.which);
+}
+
+console.log(collectionArray);
+});
