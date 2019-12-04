a63b642dbe29f3dc1436a396fb35e6c33d0e7dc7
Angela Weber <angelaweber95@hotmail.com>
2019-12-04 10:53:57 -0800

    add count down timer to also up date score

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import './styles.css';
 import './src-noconflict/ace.js';
 import './src-noconflict/theme-monokai.js';
 import './src-noconflict/mode-javascript.js';
-import './src-noconflict/worker-javascript.js';
-import { Game } from './game.js'
+// import './src-noconflict/worker-javascript.js';
+import { Game } from './game.js';
 
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
@@ -19,25 +19,29 @@ function switchQuestions(game){
   //should switch questions if answer is correct or time runs out on this.starterScore;
   for(i = 1; i <= 10; i++){
     $(`#${i}`).show();
-    $(`#${i}`).hide();
-
   }
 }
 
-function returnScore(){
-  //display current score
-}
 
 function countDownTimer(game){
-  setInterval(() => {
+  let displayCounter = setInterval(() => {
     $('h1#countDown').text(`Timer: ${game.starterScore}`);
+    $('.score').html(`Score: ${game.score}`);
   }, 10);
 }
+function returnScore(game){
+  $('.score').html(`Score: ${game.score}`);
+}
+//setTimeout(switchQuestion, 10) --hoping this will make the function run 10 time for each question. but that would just make the loop do it's 10 loops, 10 times.
 
 $(document).ready(function(){
   let collectionArray = [];
   let game = new Game(collectionArray);
-  //setTimeout(switchQuestion, 10) --hoping this will make the function run 10 time for each question. but that would just make the loop do it's 10 loops, 10 times.
+  $('#game').show();
+  console.log(game);
+  countDownTimer(game);
+  game.copyCountDown();
+  returnScore(game);
   $(document).keyup(function (event) {
     collectionArray.push(event.keyCode);
     console.log(collectionArray);
