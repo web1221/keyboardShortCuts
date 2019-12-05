dbdbc1b619ebe646e706bcd7b88002e1b14666f2
web1221 <angelaweber95@hotmail.com>
2019-12-05 08:28:26 -0800

    add switch questions function from Bryon

--- src/main.js
+++ src/main.js
@@ -14,13 +14,50 @@ editor.setTheme("ace/theme/monokai");
 editor.session.setMode("ace/mode/javascript");
 
 function switchQuestions(game){
-  //hide previous questions
-  //show new
-  //should switch questions if answer is correct or time runs out on this.starterScore;
-  for(i = 1; i <= 10; i++){
-    $(`#${i}`).show();
+  let goal;
+
+  let goalPicker = 1;
+
+  // (Math.floor(Math.random() * Math.floor(12))) + 1;
+
+  if (goalPicker === 1) {
+    goal = "Copy";
+    game.copyCountDown();
+  }else if (goalPicker === 2) {
+    goal = "Paste";
+    game.pasteCountDown();
+  }else if (goalPicker === 3) {
+    goal = "Select all"
+    game.selectAllCountDown();
+  }else if (goalPicker === 4) {
+    goal = "Toggle Comments";
+    game.toggleCommentsCountDown();
+  }else if (goalPicker === 5) {
+    goal = "Cut"
+
+
+  }else if (goalPicker === 6) {
+    goal = "Delete Line";
+  }else if (goalPicker === 7) {
+    goal = "Go to beginning of line"
+  }else if (goalPicker === 8) {
+    goal = "Go to beginning of line";
+  }else if (goalPicker === 9) {
+    goal = "Cut to the end of line"
+  }else if (goalPicker === 10) {
+    goal = "Select next match";
+  }else if (goalPicker === 11) {
+    goal = "Save";
+  }else if (goalPicker === 12) {
+    goal = "Select all";
   }
+
+
+$("#goalPlace").text(goal);
+$("#displayGoal").show();
+
 }
+
 
 
 function countDownTimer(game){
@@ -47,6 +84,7 @@ $(document).ready(function(){
 
 });
 // For nav bars -------
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -61,3 +99,110 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+// atom buttons that link to pop up banner
+$("#moveCurrent").click(function(event){
+  event.preventDefault();
+  $(".noShow").hide();
+  $("#pointer").fadeIn();
+  $("#shortcut1").fadeIn();
+
+})
+
+$("#selectNext").click(function(event){
+  event.preventDefault();
+  $(".noShow").hide();
+  $("#pointer").fadeIn();
+  $("#shortcut2").fadeIn();
+})
+
+$("#selectAll").click(function(event){
+  event.preventDefault();
+  $(".noShow").hide();
+  $("#pointer").fadeIn();
+  $("#shortcut3").fadeIn();
+})
+
+$("#shakeSelect").click(function(event){
+  event.preventDefault();
+  $(".noShow").hide();
+  $("#pointer").fadeIn();
+  $("#shortcut4").fadeIn();
+})
+// end atom buttons to pop up banner
+
+
+
+
+
+
+// chrome buttons to pop up banner
+$("#clearConsole").click(function(event){
+  event.preventDefault();
+  $(".noShow").hide();
+  $("#pointer").fadeIn();
+  $("#shortcut5").fadeIn();
+
+})
+
+$("#openInspector").click(function(event){
+  event.preventDefault();
+  $(".noShow").hide();
+  $("#pointer").fadeIn();
+  $("#shortcut6").fadeIn();
+
+})
+
+$("#newTab").click(function(event){
+  event.preventDefault();
+  $(".noShow").hide();
+  $("#pointer").fadeIn();
+  $("#shortcut7").fadeIn();
+})
+
+$("#newWindow").click(function(event){
+  event.preventDefault();
+  $(".noShow").hide();
+  $("#pointer").fadeIn();
+  $("#shortcut8").fadeIn();
+})
+
+// end chrome buttons to pop up banner
+
+
+
+
+
+// general buttons to pop up banner
+$("#copy").click(function(event){
+  event.preventDefault();
+  $(".noShow").hide();
+  $("#pointer").fadeIn();
+  $("#shortcut9").fadeIn();
+
+})
+
+$("#paste").click(function(event){
+  event.preventDefault();
+  $(".noShow").hide();
+  $("#pointer").fadeIn();
+  $("#shortcut10").fadeIn();
+
+})
+
+$("#pasteWithout").click(function(event){
+  event.preventDefault();
+  $(".noShow").hide();
+  $("#pointer").fadeIn();
+  $("#shortcut11").fadeIn();
+})
+
+$("#select").click(function(event){
+  event.preventDefault();
+  $(".noShow").hide();
+  $("#pointer").fadeIn();
+  $("#shortcut12").fadeIn();
+})
+
+// end general buttons to pop up banner
